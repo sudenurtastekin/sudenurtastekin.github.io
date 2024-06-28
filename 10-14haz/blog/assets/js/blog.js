786b41f836fa4968e075a992ff77c08716f262ca
sudenurtastekin <164672438+sudenurtastekin@users.noreply.github.com>
2024-06-28 03:12:16 +0300

    Update blog.js

--- 10-14haz/blog/assets/js/blog.js
+++ 10-14haz/blog/assets/js/blog.js
@@ -1,90 +1,126 @@
-let todoForm = document.querySelector('.todoForm');
-let todos = document.querySelector('.todos');
-let clear = document.querySelector('.clear');
-let todoCount = document.querySelector('.todoCount');
-let modal = document.getElementById('modal');
+let posts = [];
+let id = 0;
 
-let todosList = [];
-let todoCounter = 0;
-
-if (localStorage.todosList) {
-  todosList = JSON.parse(localStorage.todosList);
-  todoCounter = todosList.length;
-  todoCount.innerText = `${todoCounter}`;
-  renderList();
+if (localStorage.posts) {
+  posts = JSON.parse(localStorage.posts);
+  renderPosts();
 }
 
-function handleFormSubmit(e) {
-  e.preventDefault();
-  let formData = new FormData(todoForm);
-  let formObj = Object.fromEntries(formData);
-  todosList.push(formObj);
-  todoForm.reset();
-  todoCounter++;
-  todoCount.innerText = `${todoCounter}`;
-  renderList();
-  save();
+if (localStorage.id) {
+  id = Number(localStorage.id);
 }
 
-todoForm.addEventListener('submit', handleFormSubmit);
-
-function renderList() {
-  todos.innerHTML = '';
-  for (let i = 0; i < todosList.length; i++) {
-    let todo = todosList[i].todo;
-    todos.innerHTML +=
-      `<div class="todos-list">
-          <ul><li><p>${todo} 
-          <button class="edit" onclick="openEditDialog(${i})">🖋️</button>
-           <button class="delete" onclick="deleteTodo(${i})">🚮</button>
-           </ul></p></li>
-       </div>`;
-  }
+function generateId() {
+  id++;
+  localStorage.id = id;
+  return id;
 }
 
 function save() {
-  localStorage.todosList = JSON.stringify(todosList);
+  localStorage.posts = JSON.stringify(posts);
 }
 
-function clearList() {
-  if(!confirm('Emin misin?')) {
-    return;
-  }
+function renderPosts() {
+  allPosts.innerHTML = '';
+  categoryPosts.innerHTML = '';
 
-  localStorage.clear();
-  todosList = [];
-  todoCounter = 0;
-  todoCount.innerText = `${todoCounter}`;
-  renderList();
-}
+  let categoryLists = {};
 
-clear.addEventListener('click', clearList);
+  posts.forEach(post => {
+    let listItem = `<li data-postid="${post.id}">
+            <img src="${post.image}" alt="${post.title}" style="width: 50px; height: 50px;">
+            ${post.title} - ${post.author} 
+            <a href="#" class="editPostBtn" data-postid="${post.id}">Düzenle</a>
+            <a href="#" class="deletePostBtn" data-postid="${post.id}">Sil</a>
+        </li>`;
+    allPosts.innerHTML += listItem;
 
-function openEditDialog(index) {
-  modal.querySelector('.modelEdit').innerText = "Todo Düzenle";
-
-  let todoInput = modal.querySelector('input[name="todos"]');
-  todoInput.value = todosList[index].todo;
-
-  modal.querySelector('form').addEventListener('submit', function (e) {
-    e.preventDefault();
-    
-    todosList[index].todo = todoInput.value;
-    renderList();
-    save();
-    modal.close();
+    if (!categoryLists[post.category]) {
+      categoryLists[post.category] = [];
+    }
+    categoryLists[post.category].push(`<li><img src="${post.image}" alt="${post.title}" style="width: 50px; height: 50px;"> ${post.title} - ${post.author} <p>Açıklama:  ${post.description} </p> </li>`);
   });
 
+  posts.forEach(post => {
+    if (categoryLists[post.category]) {
+      let categoryHTML = `<h3>${post.category.toUpperCase()}</h3><ul>`;
+      categoryLists[post.category].forEach(item => {
+        categoryHTML += item;
+      });
+      categoryHTML += '</ul>';
+      categoryPosts.innerHTML += categoryHTML;
+      delete categoryLists[post.category];
+    }
+  });
+
+  document.querySelectorAll('.editPostBtn').forEach(btn => btn.addEventListener('click', handleEditBtn));
+  document.querySelectorAll('.deletePostBtn').forEach(btn => btn.addEventListener('click', handleDeleteBtn));
+}
+
+addPostBtn.addEventListener('click', () => {
+  modal.classList.remove('editModal');
+  document.querySelector('input[name="id"]').value = "";
+  modal.showModal();
+});
+
+blogForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+  let formData = new FormData(blogForm);
+  let formObj = Object.fromEntries(formData);
+  blogForm.reset();
+
+  if (formObj.id !== '') { // güncelle
+    let post = posts.find(x => x.id === Number(formObj.id));
+    post.title = formObj.title;
+    post.description = formObj.description;
+    post.author = formObj.author;
+    post.category = formObj.category;
+    post.image = formObj.image;
+  } else { // yeni ekle
+    formObj.id = generateId();
+    posts.push(formObj);
+  }
+
+  save();
+  renderPosts();
+  modal.close();
+});
+
+document.querySelector('#clearLocalStorageBtn').addEventListener('click', function () {
+  if (!confirm('Emin misin?')) {
+    return;
+  }
+  localStorage.removeItem('posts');
+  localStorage.removeItem('id');
+  posts = [];
+  id = 0;
+  renderPosts();
+});
+
+function handleDeleteBtn(e) {
+  e.preventDefault();
+
+  if (!confirm('Emin misin?')) {
+    return;
+  }
+  posts = posts.filter(x => x.id !== Number(this.dataset.postid));
+
+  save();
+  renderPosts();
+}
+
+function handleEditBtn(e) {
+  e.preventDefault();
+  modal.classList.add('editModal');
+  let postId = Number(this.dataset.postid);
+  let post = posts.find(x => x.id === postId);
+  document.querySelector('input[name="id"]').value = post.id;
+  document.querySelector('input[name="title"]').value = post.title;
+  document.querySelector('textarea[name="description"]').value = post.description;
+  document.querySelector('input[name="author"]').value = post.author;
+  document.querySelector('select[name="category"]').value = post.category;
+  document.querySelector('input[name="image"]').value = post.image;
   modal.showModal();
 }
 
-function deleteTodo(index) {
-  if(!confirm('Emin misin?')) {
-    return;
-  }
-  todosList.splice(index, 1);
-  todoCounter--;
-  todoCount.innerText = `${todoCounter}`;
-  renderList();
-  save();
-}
+renderPosts();
